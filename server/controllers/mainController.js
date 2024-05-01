61d1e782a92e6ae7ee1f8729a8957a8543d37bc1
Sophie Théréau <sophiethereau@hotmail.fr>
2024-05-01 22:24:31 +0200

    création des partials header et footer

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -6,3 +6,13 @@ exports.homepage = async (req, res) => {
     };
     res.render('index', locals);
 }
+
+
+// Get About page
+exports.about = async (req, res) => {
+    const locals = {
+        title: "A propos - Notes NodeJS",
+        description: "Application de notes sous NodeJS"
+    };
+    res.render('about', locals);
+}
